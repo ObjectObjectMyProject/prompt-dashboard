755275e8f2044c3077c3aa78a99cdd71dfcff84b
Mikita <sevenscoding@gmail.com>
2018-04-26 02:47:22 +0300

    Add main page for project

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
-console.log('\'Allo \'Allo!');
+
+$('.owl-carousel').owlCarousel({
+  loop: false,
+  nav: false,
+  responsive:{
+    0:{
+      items:1
+    },
+    600:{
+      items:1
+    },
+    1000:{
+      items:1
+    }
+  }
+});
